f090d13752976be029070584d44b7d50e209ea36
Marian <hellomariandev@gmail.com>
2023-05-08 15:51:01 +0200

    managing books with zustand

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,13 +1,47 @@
 //? introduction to application with explanation
 
+import { useEffect } from "react";
+import useBookStore from "../store/useBookStore";
+
 const HomePage = () => {
+  const books = useBookStore((state) => state.books);
+  const fetchBooks = useBookStore((state) => state.fetchBooks);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
+
+  console.log(books);
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>introduction to application with explanation</p>
-      <div className="introduction"></div>
-      <div className="example-books"></div>
-      <div className="example-lists"></div>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ margin: "40px " }}>Home Page</h1>
+        {books.map((book) => (
+          <div
+            key={book.id}
+            style={{
+              border: "1px black solid",
+              margin: "0.9rem",
+              padding: "0.5rem",
+              minWidth: "400px",
+            }}
+          >
+            <div>
+              <h3>{book.title}</h3>
+            </div>
+            <h4>{book.author}</h4>
+          </div>
+        ))}{" "}
+      </div>
     </div>
   );
 };
