974563d41487d8c466a2557f37c8f2538073ff30
Marian <hellomariandev@gmail.com>
2023-05-14 23:45:47 +0200

    adding function

--- store/createBookSlice.js
+++ store/createBookSlice.js
@@ -1,9 +1,25 @@
 const createBookSlice = (set, get) => ({
   books: [],
+  personalBooks: [],
   fetchBooks: async () => {
     const res = await fetch("http://localhost:3000/books");
     set({ books: await res.json() });
   },
+  addBook: (book) =>
+    set({
+      personalBooks: [...new Set([...get().personalBooks, book])]
+    }),
 });
 
-export default createBookSlice
+{
+  /*
+
+      personalBooks: [...get().personalBooks, book]
+        => this adds same user multiple times
+            we have changed it with Set object (makes sure that data in array is unique)
+            then spreading the value of Set to new array
+
+    */
+}
+
+export default createBookSlice;
