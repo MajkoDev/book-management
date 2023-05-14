6786241f5940c156313b15c607e64c78af80ecfb
Marian <hellomariandev@gmail.com>
2023-05-14 23:46:10 +0200

    finished

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,47 +1,79 @@
-//? introduction to application with explanation
+//? displaying books in react-table
 
-import { useEffect } from "react";
-import useBookStore from "../store/useBookStore";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import BookItem from "../components/BookItem";
 
 const HomePage = () => {
-  const books = useBookStore((state) => state.books);
-  const fetchBooks = useBookStore((state) => state.fetchBooks);
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
 
+  // asynch fetching data with axios
   useEffect(() => {
-    fetchBooks();
-  }, [fetchBooks]);
+    const getBooks = async () => {
+      try {
+        const res = await axios
+          .get("http://localhost:3000/books")
+          .then((res) => setBooks(res.data))
+          .finally(() => setLoading(false));
+      } catch (err) {
+        console.log("There has been error somewhere.");
+      }
+    };
+    setTimeout(getBooks, 1500);
+  }, []);
 
-  console.log(books);
+  // searching functionality
+  const [searchBook, setSearchBook] = useState("");
+
+  function handleSearch(e) {
+    setSearchBook(e.target.value);
+  }
+
+  const searchingBooks = books.filter((book) => {
+    return book.title.toLowerCase().includes(searchBook.toLowerCase());
+  });
 
   return (
-    <div>
-      <div
-        style={{
-          marginTop: "20px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <h1 style={{ margin: "40px " }}>Home Page</h1>
-        {books.map((book) => (
-          <div
-            key={book.id}
-            style={{
-              border: "1px black solid",
-              margin: "0.9rem",
-              padding: "0.5rem",
-              minWidth: "400px",
-            }}
-          >
-            <div>
-              <h3>{book.title}</h3>
-            </div>
-            <h4>{book.author}</h4>
-          </div>
-        ))}{" "}
+    <div className="book-page">
+      <div className="searching">
+        <h1>Books</h1>
+        <input
+          type="text"
+          placeholder="Anna Karenina"
+          value={searchBook}
+          onChange={handleSearch}
+        />
       </div>
+
+      {/*
+        <div className="displaying">
+                {JSONDATA.map((book) => (
+                <div className="book-item" key={book.id}>
+                    <div className="book-title">
+                    <div>
+                        <h3>{book.title}</h3>
+                        <h4>{book.author}</h4>
+                    </div>
+                    <h5>{book.price} €</h5>
+                    </div>
+                    <p>{book.description}</p>
+                </div>
+                ))}
+            </div>
+         */}
+
+      {loading ? (
+        <h2 className="loading">Loading</h2>
+      ) : (
+        <>
+          <div className="displaying">
+            {searchingBooks.map((book) => (
+              <BookItem book={book} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
